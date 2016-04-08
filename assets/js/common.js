386a9917c98b0a60f896645f7f9c2e4dd5943ee5
Christopher Van <cvan@mozilla.com>
2016-04-08 03:49:18 -0700

    add sweet audio (and pausing)

---
+++ assets/js/common.js
@@ -0,0 +1,84 @@
+(function () {
+
+function toArray (obj) {
+  return Array.prototype.slice.apply(obj);
+}
+
+function $ (selector, parent) {
+  parent = parent || document;
+  return parent.querySelector(selector);
+}
+
+function $$ (selector, parent) {
+  parent = parent || document;
+  return toArray(parent.querySelectorAll(selector));
+}
+
+window.addEventListener('keypress', function (e) {
+  if (e.keyCode === 112) {  // `P`
+    sounds.toggle();
+  }
+});
+
+function Sounds () {
+  var self = this;
+
+  self.muted = false;
+
+  self.toggle = function () {
+    if (self.muted) {
+      self.play();
+    } else {
+      self.stop();
+    }
+  };
+
+  self.playEl = function (el) {
+    el.components.sound.sound.autoplay = el.components.sound.sound._autoplay;
+    el.components.sound.sound.src = el.components.sound.sound._src;
+    el.components.sound.play();
+  };
+
+  self.stopEl = function (el) {
+    el.components.sound.pause();
+    el.components.sound.sound._autoplay = el.components.sound.sound._autoplay;
+    el.components.sound.sound.autoplay = false;
+    el.components.sound.sound._src = el.components.sound.sound.src;
+    el.components.sound.sound.src = '';
+  };
+
+  self.play = function () {
+    self.muted = false;
+    self.remember();
+    $$('a-scene [sound]').forEach(self.playEl);
+  };
+
+  self.stop = function () {
+    self.muted = true;
+    self.remember();
+    $$('a-scene [sound]').forEach(self.stopEl);
+  };
+
+  self.remember = function () {
+    try {
+      localStorage.audioMuted = self.muted ? 'true' : 'false';
+    } catch (e) {
+    }
+  };
+
+  self.muteOnLoad = function () {
+    try {
+      return localStorage.audioMuted === 'true';
+    } catch (e) {
+      return null;
+    }
+  };
+
+  if (self.muteOnLoad()) {
+    self.stop();
+  }
+}
+
+var sounds = new Sounds();
+
+})();
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,3 @@
 (function () {
 
-function toArray (obj) {
-  return Array.prototype.slice.apply(obj);
-}
-
-function $ (selector, parent) {
-  parent = parent || document;
-  return parent.querySelector(selector);
-}
-
-function $$ (selector, parent) {
-  parent = parent || document;
-  return toArray(parent.querySelectorAll(selector));
-}
-
 })();
